c7cf4a6fa2b7fd8c50808ae873bf7db64e661806
Garrett Swann <garrettswann3049@yahoo.com>
2019-02-07 20:42:21 -0800

    Initial commit

---
+++ webpack.config.js
@@ -0,0 +1,32 @@
+const path = require('path');
+const webpack = require('webpack');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+
+module.exports = (env) => {
+    return {
+        entry: {
+            main: ['@babel/polyfill', './src/app.js']
+        },
+        module: {
+            rules: [
+                {
+                    test: /\.js$/,
+                    exclude: /node_modules/,
+                    loaders: ['babel-loader']
+                }
+            ]
+        },
+        resolve: {
+            modules: [path.resolve(__dirname, 'src'), 'node_modules']
+        },
+        output: {
+            filename: 'main.js',
+            path: path.resolve(__dirname, 'dist')
+        },
+        plugins: [
+            new HtmlWebpackPlugin({
+                template: './src/index.html'
+            })
+        ]
+    };
+}
